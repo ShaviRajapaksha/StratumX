d1f0de8fab5da03fb1e40f4fe3a57c642184a413
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-07-25 01:00:09 +0530

    Remove unused assets and configuration files to streamline the project

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  output: "export", // 👈 Tells Next.js to generate static HTML/CSS/JS files
-  images: {
-    unoptimized: true, // Required because standard Next images need a Node server
-  },
-};
-
-export default nextConfig;
